be7eb684d3d435f64dfe8e0af2843613c2d1761d
ilabstaff <ilabstaff@172.16.116.71>
2019-06-08 10:55:01 -0700

    chuckle clicker					nasty surprise

--- src/Runners/NastySurpriseRunner.java
+++ src/Runners/NastySurpriseRunner.java
@@ -1,4 +1,4 @@
-package _04_chuckle_clicker;
+package Runners;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,26 +10,23 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-import Runners.NastySurpriseRunner;
-
-public class ChuckleClicker implements ActionListener{
+public class NastySurpriseRunner implements ActionListener{
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-	JButton one = new JButton("Joke");
-	JButton two = new JButton("Punchline");
+	JButton button1 = new JButton("Trick");
+	JButton button2 = new JButton("Treat");
 	
 public static void main(String[] args) {
-	 new ChuckleClicker().createUI();
+	 new NastySurpriseRunner().createUI();
 }
 void createUI(){
 	frame.add(panel);
-	panel.add(one);
-	panel.add(two);
-	one.addActionListener(this);
-	two.addActionListener(this);
+	panel.add(button1);
+	panel.add(button2);
+	button1.addActionListener(this);
+	button2.addActionListener(this);
 	frame.setVisible(true);
 	frame.pack();
 }
@@ -48,13 +45,13 @@ private void showPictureFromTheInternet(String imageUrl) {
 }
 @Override
 public void actionPerformed(ActionEvent e) {
-	if(e.getSource()== one) {
-		JOptionPane.showMessageDialog(null,"Hi");
-		JOptionPane.showMessageDialog(null, "When you thought you passed your test but you failed so hard your grades's a G");
+	if(e.getSource()==button1) {
+		showPictureFromTheInternet("https://cdn.kapwing.com/final_5c702376cb41e60013113413_837938.jpg");
 	}
-	else if(e.getSource()==two) {
-		JOptionPane.showMessageDialog(null, "Hi");
-		showPictureFromTheInternet("https://i.kym-cdn.com/entries/icons/mobile/000/024/062/jerry.jpg");
+	if(e.getSource()==button2) {
+		showPictureFromTheInternet("https://thumbs.gfycat.com/SelfassuredWholeJavalina-size_restricted.gif");
+	}
 	}
 }
-}
+
+
