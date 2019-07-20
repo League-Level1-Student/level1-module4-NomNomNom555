34c89ae0ec9e73f0e3a72105bcc53d97192d3637
ilabstaff <ilabstaff@172.16.116.71>
2019-07-20 10:49:21 -0700

    whack moles 24/7

--- src/Runners/IWhackMoles.java
+++ src/Runners/IWhackMoles.java
@@ -1,23 +1,75 @@
 package Runners;
 
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+import java.util.Random;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class IWhackMoles {
-void drawbuttons(int buttons){
-JFrame frame = new JFrame();
-JPanel panel = new JPanel();
-for(int i=0; i<= buttons; i++) {
+public class IWhackMoles implements ActionListener {
+	JFrame frame = new JFrame("The Whacker Smacker");
+	JPanel panel = new JPanel();
+	JButton mole = new JButton("MOLE");
+	static int buttonPressed = 0;
+	static Date timeAtStart= new Date();
+
+	public static void main(String[] args) {
+		Random sandy = new Random();
+		new IWhackMoles().drawbuttons(sandy.nextInt(23));
+		;
+	}
+void drawbuttons(int location){
+	frame.add(panel);
+for(int i=0; i<23; i++) {
 JButton b= new JButton();
-}
-frame.add(panel);
-for(int i=0; i<= buttons; i++) {
+b.addActionListener(this);
+b.setPreferredSize(new Dimension(100,45));
 panel.add(b);
+if(i==location) {
+	mole.setPreferredSize(new Dimension(100,45));
+	mole.addActionListener(this);
+	panel.add(mole);
+}
+}
+frame.setVisible(true);
+frame.setSize(700, 250);
 }
 
+
+void speak(String words) {
+    try {
+         Runtime.getRuntime().exec("say " + words).waitFor();
+    } catch (Exception e) {
+         e.printStackTrace();
+    }
 }
-void createUI() {
-	drawButtons();
+private void endGame(Date timeAtStart, int molesWhacked) {
+    Date timeAtEnd = new Date();
+    JOptionPane.showMessageDialog(null, "Your whack rate is "
+         + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+         + " moles per second.");
+}
+
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	if(e.getSource()==mole) {
+		Random sandy = new Random();
+		new IWhackMoles().drawbuttons(sandy.nextInt(23));
+		buttonPressed++;
+		System.out.println(buttonPressed);
+		if(buttonPressed==10) {
+			endGame(timeAtStart,0);
+			 System.exit(0);
+		}
+	}
+	else {
+		speak("Missed!");
+	}
 }
 }
